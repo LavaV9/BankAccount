08f1f9db5f906c73434e652d8012b95eae5b63b1
Pranav Rao <prao7@uncc.edu>
2025-02-13 19:36:43 -0500

    Created checking account

--- checking_account.py
+++ checking_account.py
@@ -1,1 +1,15 @@
 from bank_account import BankAccount
+class CheckingAccount(BankAccount):
+    def __init__(self, customer_name, current_balance, minimum_balance, account_num, routing_num, transfer_limit):
+        super().__init__(customer_name, current_balance, minimum_balance, account_num, routing_num)
+        self.transfer_limit = transfer_limit  # max amnt
+
+    def transfer(self, amount, recipient):
+        if amount > self.transfer_limit:
+            print(f"{self.customer_name}: Transfer denied. Exceeds limit of {self.transfer_limit}.")
+        elif self.current_balance - amount >= self.minimum_balance:
+            self.current_balance -= amount
+            recipient.current_balance += amount
+            print(f"{self.customer_name}: Transferred {amount} to {recipient.customer_name}. New balance: {self.current_balance}")
+        else:
+            print(f"{self.customer_name}: Transfer denied. Insufficient funds.")
