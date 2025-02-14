b85fd4024ed56f4e313c658486e4b4ccb42d4a2f
Pranav Rao <prao7@uncc.edu>
2025-02-13 19:34:06 -0500

    Improved savings account

--- savings_account.py
+++ savings_account.py
@@ -1,9 +1,11 @@
 from bank_account import BankAccount
 
 class SavingsAccount(BankAccount):
-    interest_rate = 0.05
-    interest_as_percent = interest_rate * 100
-    def inetrest_function(self):
-        interest = self.current_balance * self.interest_rate
-        self.current_balance = self.current_balance + interest
-        print("New balance with interest is",self.current balance, ". An interest rate of", interest_as_percent,"%")
+    def __init__(self, customer_name, current_balance, minimum_balance, account_num, routing_num, interest_rate):
+        super().__init__(customer_name, current_balance, minimum_balance, account_num, routing_num)
+        self.interest_rate = interest_rate  # whole number that is later made to be percentage
+
+    def apply_interest(self):
+        interest = self.current_balance * (self.interest_rate / 100)
+        self.current_balance += interest
+        print(f"{self.customer_name}: Interest applied at {self.interest_rate}%. New balance: {self.current_balance}")
